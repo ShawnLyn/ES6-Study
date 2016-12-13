69e29d37e4c8f82baef39990f9814bc633e183a1
liang xiang hui <552594996@qq.com>
2016-12-13 17:30:44 +0800

    s

--- You-Don't-Know-Js/s3-closure.js
+++ You-Don't-Know-Js/s3-closure.js
@@ -76,4 +76,28 @@
 //   }, i*1000)
 // }
 
-// 页码66
+// function CoolModule() {
+// 	var something = 'cool';
+//
+// 	var another = [1, 2, 3];
+//
+// 	function doSomething() {
+// 		console.log(something)
+// 	}
+//
+// 	function doAnother() {
+// 		console.log(another.join('!'))
+// 	}
+//
+// 	return {
+// 		doSomething: doSomething,
+// 		doAnother: doAnother
+// 	}
+// }
+//
+// var foo = CoolModule()
+// foo.doSomething();
+// foo.doAnother();
+
+// console.log(Math.max.apply(Math, [1,2,3]))
+// 69
