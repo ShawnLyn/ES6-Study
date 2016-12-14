2959ed533f9597bc56815b2f5f756d020a334f31
liang xiang hui <552594996@qq.com>
2016-12-14 15:41:18 +0800

    s

--- I-write-JS/Write.js
+++ I-write-JS/Write.js
@@ -1,4 +1,9 @@
 // 整数型数组如何求出最大值
 //Math.max.apply(null, [1,2,3])
 
-
+// 将类数组变为数组的方法，比如函数的 arguments
+// var a = {0:'a', 1:'b', 2:'c', length:3}
+// console.log(
+// 	Array.prototype.slice.apply(a),
+// 	Array.prototype.slice.call(a)
+// )
---
+++ You-Don't-Know-Js/s4-this.js
@@ -0,0 +1,32 @@
+// function foo(num) {
+//
+// 	console.log('foo:' + num);
+// 	this.count++;
+// }
+//
+// foo.count = 0;
+//
+// for (var i=0; i<5; i++) {
+// 	foo(i)
+// }
+//
+// console.log(foo.count)
+
+// function foo(num) {
+// 	console.log('foo:' + num);
+// 	data.count++;
+// }
+//
+// var data = {
+// 	count: 0
+// }
+//
+// foo.count = 0;
+//
+// for (var i=0; i<5; i++) {
+// 	foo(i)
+// }
+//
+// console.log(data.count)
+
+// 页码93
