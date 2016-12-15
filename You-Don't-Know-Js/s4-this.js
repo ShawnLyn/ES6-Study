5eab238a048d27d5074433d5c0c988ae536c308e
minooo <552594996@qq.com>
2016-12-15 22:14:44 +0800

    s

--- You-Don't-Know-Js/s4-this.js
+++ You-Don't-Know-Js/s4-this.js
@@ -29,4 +29,80 @@
 //
 // console.log(data.count)
 
-// 页码93
+// this 在任何情况下都不指向函数的词法作用域
+// function foo() {
+//   var a = 2;
+//   this.bar();
+// }
+//
+// function bar() {
+//   console.log(a)
+// }
+//
+// foo()
+
+// this 既不指向函数自身，也不指向函数的词法作用域
+// this 实际上是函数被调用时发生的绑定，它指向什么取决于函数在哪被调用
+
+// function baz() {
+//   // 当前调用栈 baz
+//   // 当前的调用位置是全局作用域
+//
+//   console.log("baz")
+//   bar(); // bar的调用位置
+// }
+//
+// function bar() {
+//   // 当前调用栈 baz -> bar
+//   // 当前调用位置是 baz
+//
+//   console.log("bar")
+//   foo(); // foo 的调用位置
+// }
+//
+// function foo() {
+//   // 当前的调用栈 baz -> bar -> foo
+//   // 当前的调用位置是 bar
+//
+//   console.log('foo')
+// }
+//
+// baz() // baz的调用位置
+
+// 1.默认绑定
+// 即：不带任何修饰的函数调用。
+// function foo() {
+//   console.log(a)
+// }
+//
+// var a = 2;
+//
+// foo()
+
+// 2.隐式绑定
+// 对象属性引用链中只有最后一层会影响函数调用位置
+
+// function foo() {
+//   console.log(this.a)
+// }
+//
+// let a = 2;
+//
+// foo.apply({a:123})
+
+// function foo() {
+//   console.log(this.a)
+// }
+//
+// var obj = {
+//   a: 2,
+//   foo: foo
+// }
+//
+// var a = 11;
+//
+// setTimeout(obj.foo, 100)
+
+// 3.隐式丢失 回调函数会隐式this,比如著名的 setTimeout
+
+// 页码 103
