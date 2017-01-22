374ff6a2ae82cd76c5ddddd5b96f4df53140cdae
liang xiang hui <552594996@qq.com>
2017-01-22 17:34:52 +0800

    s

--- I-write-JS/Write.js
+++ I-write-JS/Write.js
@@ -7,3 +7,19 @@
 // 	Array.prototype.slice.apply(a),
 // 	Array.prototype.slice.call(a)
 // )
+
+function draw(m=2,n=1) {
+	let
+		arr = [],
+		myArr = new Array(m).fill(1).map((item, index) => index+1);
+
+	if(n > m){console.log('抽取人数不能大于总人数'); return}
+
+	for (let i=1; i <= n; i++) {
+		const ran = Math.floor(myArr.length * Math.random());
+		arr.push(...myArr.splice(ran, 1));
+	}
+	console.log(arr)
+}
+
+draw(4,3);
